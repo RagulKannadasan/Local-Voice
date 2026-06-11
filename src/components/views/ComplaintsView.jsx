bb55409843faf2f82573427efc26e30f9f2461b4
Ragulkannadasan <ragulkavai@gmail.com>
2026-06-11 16:02:00 +0530

    image upload in complients

--- src/components/views/ComplaintsView.jsx
+++ src/components/views/ComplaintsView.jsx
@@ -81,7 +81,7 @@ export default function ComplaintsPage() {
                   height = MAX_HEIGHT;
                 }
               }
-              
+              const canvas = document.createElement('canvas');
               canvas.width = width;
               canvas.height = height;
               const ctx = canvas.getContext('2d');
